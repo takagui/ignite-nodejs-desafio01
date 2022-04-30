5abe8d187b50c8daca3dd80aa08f7055926c9a57
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-04-30 14:24:54 -0300

    feat: remove todo

--- src/index.js
+++ src/index.js
@@ -103,7 +103,18 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: 'Todo does not exist' });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
