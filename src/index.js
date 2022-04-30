1a65d2534bcbdca65258b3ee36272a8b0964152e
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-04-30 14:17:07 -0300

    feat: update todo

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const todo = user.todos.find(todo => todo.id === id);
 
   if (!todo) {
-    return response.status(404).json({ error: 'Todo already exists '});
+    return response.status(404).json({ error: 'Todo does not exist' });
   }
 
   todo.title = title;
@@ -88,7 +88,18 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo does not exist' });
+  }
+
+  todo.done = !todo.done;
+
+  return response.status(200).json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
