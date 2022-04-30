b032a922dfa21a76def40fe90039df7350ecbc6d
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-04-30 14:08:26 -0300

    feat: change todo

--- src/index.js
+++ src/index.js
@@ -71,7 +71,16 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(200).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
